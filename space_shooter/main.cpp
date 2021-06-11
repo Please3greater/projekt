e06d080322f6115ec5e26e943b2c9bf759d3a396
Please3greater <76478549+Please3greater@users.noreply.github.com>
2021-06-11 17:56:54 +0200

    start
    start

---
+++ space_shooter/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+
+void projekt()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    projekt();
+    return 0;
+}
